d1dfc6c81a077cbf5491c23c9edb6706fa656e4e
Juan Mauel Britos <britosjuanmanuel@gmail.com>
2017-12-01 14:35:15 -0300

    FermenterDataCollection1

--- app/src/main/java/ar/edu/proyectosort/brewin/MainActivity.java
+++ app/src/main/java/ar/edu/proyectosort/brewin/MainActivity.java
@@ -1,13 +1,29 @@
 package ar.edu.proyectosort.brewin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn = (Button)findViewById(R.id.btnFermenter);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+
+        if(view.getId()== R.id.btnFermenter){
+            Intent i = new Intent(MainActivity.this,FermenterActivity.class);
+            startActivity(i);
+        }
+
+    }
+
 }
